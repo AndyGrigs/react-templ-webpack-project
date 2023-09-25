7979fa2204eed031507751f800e421f0d1b17ede
AndyGrigs <andygrigs88@gmail.com>
2023-09-25 15:05:06 +0200

    adding testing with translation

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,10 +1,12 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import { useTranslation, withTranslation } from 'react-i18next';
+import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
 
 describe('Button', () => {
     test('with only first param', () => {
-        render(<Sidebar/>);
+       renderWithTranslation(<Sidebar/>)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 });
